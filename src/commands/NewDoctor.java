7453028ff9abdcda526d884d9ab83f6386024351
Shea Slade <sheaslade@gmail.com>
2019-10-30 20:06:51 -0600

    Added modified method code from Hospital System

--- src/commands/NewDoctor.java
+++ src/commands/NewDoctor.java
@@ -1,19 +1,14 @@
 package commands;
 
 import commands.CommandStatus;
+import containers.DoctorMapAccess;
+import entities.Doctor;
+import entities.Surgeon;
 
 import java.util.Scanner;
 
 public class NewDoctor extends CommandStatus
 {
-
-
-
-
-
-
-
-
     public void addDoctor()
     {
         Scanner consoleIn = new Scanner(System.in);
@@ -21,7 +16,8 @@ public class NewDoctor extends CommandStatus
         System.out.println("Getting entities.Patient information...");
         System.out.print("Enter the name of the doctor: ");
         String name = consoleIn.nextLine();
-        if (doctors.containsKey(name))
+        if(DoctorMapAccess.dictionary().containsKey(name))
+        //if (doctors.containsKey(name))
             throw new RuntimeException("entities.Doctor not added as there already "
                     + "is a doctor with the name " + name);
 
@@ -34,16 +30,15 @@ public class NewDoctor extends CommandStatus
             d = new Doctor(name);
 
         // check to make sure the doctor name doesn't already exsist
-        Doctor sameNumberDoctor = doctors.put(name, d);
+        Doctor sameNumberDoctor = (Doctor) DoctorMapAccess.dictionary().put(name, d);
+        //Doctor sameNumberDoctor = doctors.put(name, d);
         if (sameNumberDoctor != null)
         {
-            doctors.put(name, sameNumberDoctor); // put the original doctor back
+            DoctorMapAccess.dictionary().put(name, sameNumberDoctor);
+            //doctors.put(name, sameNumberDoctor); // put the original doctor back
             throw new RuntimeException("Name in the doctor dictionary even though "
                     + "containsKey failed.  Name "  + name + " not entered.");
         }
     }
-
-
-
-
 }
+
